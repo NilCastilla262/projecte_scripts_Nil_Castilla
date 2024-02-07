7436feefa7281406f07700c7f4d6b24abbe25664
Nil Castilla <ncastilla262@boscdelacoma.cat>
2024-02-07 20:33:02 +0100

    Guardar en 2 variables els alpine d'els scripts, en una el que està funcionant i en l'altre el parat

--- connectar_powershell.ps1
+++ connectar_powershell.ps1
@@ -1,3 +1,6 @@
 Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
 Connect-VIServer -Server 172.24.20.111 -User root -Password Patata123*
-$vms=Get-VM | Format-List
+$alpine_bo=Get-VM | Where-Object { ($_.Name -like 'alpine_script*') -and ($_.PowerState -eq 'PoweredOn') } | Format-List
+$alpine_copy=Get-VM | Where-Object { ($_.Name -like 'alpine_script*') -and ($_.PowerState -eq 'PoweredOff') } | Format-List
+
+
