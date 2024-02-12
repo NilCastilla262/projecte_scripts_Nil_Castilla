994140e7cc8d1569b742320f75bc23c43e17221d
Nil Castilla <ncastilla262@boscdelacoma.cat>
2024-02-12 16:27:05 +0100

    MV Apline creada desde script

--- connectar_powershell.ps1
+++ connectar_powershell.ps1
@@ -1,28 +1,22 @@
-function crearVM {
-    param ()
+function crearAlpine {
+    # Variables
+    $VMName = "Alpine"
+    $Datastore = "datastoreBSD"
+    $DiskGB = 16
+    $MemoryGB = 1
+    $NumCpu = 1
+    $NetworkName = "VLAN24"
 
-    #Crear vm alpine
-    #Info de la màquina
-    $vmName = "AlpineApacheVM"
-    $datastore = "datastoreBSD"
-    $isoPath = "isos/alpine-standard-3.19.1-x86_64.iso"
+    # Creació de la MV
+    New-VM -Name $VMName -Datastore $Datastore -DiskGB $DiskGB -MemoryGB $MemoryGB -NumCpu $NumCpu -GuestId "otherLinux64Guest"
 
-    # Crear la vm
-    New-VM -Name $vmName -ResourcePool (Get-Cluster).ResourcePool -Datastore $datastore -CD -DiskGB 20 -MemoryGB 2 -NumCpu 1 -GuestId "Debian GNU/Linux 10 (64-bit)" -Version "vmx-13"
-
-    # Conectar la ISO a la vm
-    Set-CDDrive -VMHost $vmName -IsoPath $isoPath -StartConnected $true
-
-    # Engegar la vm
-    Start-VM -VM $vmName
+    $networkAdapter = Get-VM $VMName | Get-NetworkAdapter
+    Set-NetworkAdapter -NetworkAdapter $networkAdapter -NetworkName $NetworkName
 }
-
-
 
 Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false >>/dev/null
 Connect-VIServer -Server 172.24.20.111 -User root -Password Patata123* >>/dev/null
-$alpine_bo=Get-VM | Where-Object { ($_.Name -like 'alpine_script*') -and ($_.PowerState -eq 'PoweredOn') } | Format-List
-$alpine_copy=Get-VM | Where-Object { ($_.Name -like 'alpine_script*') -and ($_.PowerState -eq 'PoweredOff') } | Format-List
+#$alpine_bo=Get-VM | Where-Object { ($_.Name -like 'alpine_script*') -and ($_.PowerState -eq 'PoweredOn') } | Format-List
+#$alpine_copy=Get-VM | Where-Object { ($_.Name -like 'alpine_script*') -and ($_.PowerState -eq 'PoweredOff') } | Format-List
 
-
-crearVM
+crearAlpine
