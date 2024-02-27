02e0b67ee7d669123e135cf4b68b5c61ff9c8b98
Nil Castilla <ncastilla262@boscdelacoma.cat>
2024-02-27 17:17:36 +0100

    He ficat l'arxiu on utilitzavem la classe BD creada anteriorment

---
+++ testclass.py
@@ -0,0 +1,17 @@
+import Class.BD as BD
+
+connexio=BD.connect()
+
+result=BD.insertRow(connexio, ["PC1", "172.19.1.15"])
+if result == 1:
+    print(f"Insert {result} row(s)")
+else:
+    print(f"Error: {result}")
+
+result=BD.updateRow(connexio, "172.19.1.15", "PC2")
+
+result=BD.deleteRow(connexio, "172.19.1.15")
+
+result = BD.selectALL(connexio)
+BD.imprimir(result)
+BD.close(connexio)
