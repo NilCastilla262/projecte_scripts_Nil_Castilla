31254c171bcd9eb2c1e4b62f201d77dc62d47ef5
Nil Castilla <ncastilla262@boscdelacoma.cat>
2024-02-27 17:33:02 +0100

    He creat un nou arxiu que es diu info_maquines.ps1 que sera el que comprovara si la info esta en la BD o si ha canviat.

---
+++ info_maquines.ps1
@@ -0,0 +1,28 @@
+Import-Module VMware.VimAutomation.Core
+
+#Constants
+$LOGDIR="/var/log/projecte.log"
+#Fconstants
+
+#Funcions
+
+function connectar {
+    param (
+        
+    )
+    Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false >>/dev/null
+    $connexio_nil = Connect-VIServer -Server 172.24.20.3 -User administrator@vsphere.local -Password Patata1234*
+    return $connexio_nil
+}
+
+function desconnectar {
+    param (
+        $connexio
+    )
+
+    Disconnect-VIServer -Server $connexio.Name -Confirm:$false
+}
+
+connectar
+
+desconnectar
